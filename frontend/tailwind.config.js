3b8de53cc67197355194bf839396eb873376c8c2
tayoolaigbe <tayoolaigbe@outlook.com>
2023-05-01 23:20:41 +0100

    Changed the tailwindcss config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#1565D8',
+				dark: {
+					light: '#5A7184',
+					hard: '#0D2436',
+					soft: '#183B56',
+				},
+			},
+			fontFamily: {
+				opensans: ["'Open Sans'", 'sans-serif'],
+				roboto: ["'Roboto'", 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 };
