d3cb4ce9cd0f2a07917285b1ea4766431594c630
tayoolaigbe <tayoolaigbe@outlook.com>
2023-05-01 23:44:04 +0100

    Working on making the Navigation section to be responsive

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { images } from '../contants';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const NavItemInfo = [
 	{ name: 'Home' },
@@ -22,14 +24,33 @@ const NavItem = ({ name }) => {
 };
 
 const Header = () => {
+	const [navIsVisible, setNavIsVisible] = useState(false);
+
+	const navVisibilityHandler = () => {
+		setNavIsVisible(currentState => !currentState);
+	};
 	return (
 		<section>
 			<header className="container mx-auto px-5 flex justify-between py-4 items-center">
 				<div>
-					<img src={images.Logo} alt="logo" />
+					<img className="w-16" src={images.Logo} alt="logo" />
 				</div>
-				<div className="flex gap-x-9 items-center">
-					<ul className="flex gap-x-2 font-semibold">
+				<div className="z-50">
+					{navIsVisible ? (
+						<AiOutlineClose
+							className="w-6 h-6"
+							onClick={navVisibilityHandler}
+						/>
+					) : (
+						<AiOutlineMenu className="w-6 h-6" onClick={navVisibilityHandler} />
+					)}
+				</div>
+				<div
+					className={`${
+						navIsVisible ? 'right-0' : '-right-full'
+					} mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+				>
+					<ul className="flex flex-col lg:flex-row gap-x-2 font-semibold">
 						{NavItemInfo.map(item => (
 							<NavItem key={item.name} name={item.name} />
 						))}
