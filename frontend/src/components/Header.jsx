27bd8d48ed6c79e47d6b68f9e6c4acc45f66f17b
tayoolaigbe <tayoolaigbe@outlook.com>
2023-05-05 20:57:01 +0100

    Improved the Header component mobile view

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -28,6 +28,11 @@ const NavItemInfo = [
 ];
 
 const NavItem = ({ item }) => {
+	const [dropdown, setDropdown] = useState();
+
+	const toggleDropdownHandler = () => {
+		setDropdown(prevState => !prevState);
+	};
 	return (
 		<li className="relative group">
 			{item.type === 'link' ? (
@@ -35,18 +40,25 @@ const NavItem = ({ item }) => {
 					<a href="/" className="px-4 py-2">
 						{item.name}
 					</a>
-					<span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+					<span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
 						/
 					</span>
 				</>
 			) : (
-				<>
-					<a href="/" className="px-4 py-2 flex gap-x-1 items-center">
+				<div className="flex flex-col items-center">
+					<button
+						className="px-4 py-2 flex gap-x-1 items-center"
+						onClick={toggleDropdownHandler}
+					>
 						<span>{item.name}</span>
 						<MdKeyboardArrowDown />
-					</a>
-					<div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
-						<ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+					</button>
+					<div
+						className={`${
+							dropdown ? 'block' : 'hidden'
+						} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+					>
+						<ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
 							{item.items.map(page => (
 								<a
 									key={page}
@@ -58,7 +70,7 @@ const NavItem = ({ item }) => {
 							))}
 						</ul>
 					</div>
-				</>
+				</div>
 			)}
 		</li>
 	);
