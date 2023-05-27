2f56e5ea81ad76756a66f7c3ae27c02012057ecd
tayoolaigbe <tayoolaigbe@outlook.com>
2023-05-27 23:42:06 +0100

    Implemented the isLoading &  Error state functionality for ArticleDetail page

--- frontend/src/pages/articleDetail/ArticleDetail.jsx
+++ frontend/src/pages/articleDetail/ArticleDetail.jsx
@@ -17,6 +17,8 @@ import Italic from '@tiptap/extension-italic';
 import Document from '@tiptap/extension-document';
 import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
+import ArticleDetailSkeleton from './components/ArticleDetailSkeleton';
+import ErrorMessage from '../../components/ErrorMessage';
 
 const postsdata = [
 	{
@@ -96,57 +98,63 @@ const ArticleDetail = () => {
 	});
 	return (
 		<MainLayout>
-			<section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
-				<article className="flex-1">
-					<BreadCrumbs data={breadCrumbsData} />
-					<img
-						className="rounded-xl w-full"
-						src={
-							data?.photo
-								? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
-								: images.SamplePostImage
-						}
-						alt={data?.title}
-					/>
-					<div className="mt-4 flex gap-3">
-						{data?.categories?.map(category => (
-							<Link
-								key={category?.name}
-								to={`/blog?category=${category?.name}`}
-								className="text-primary text-sm font-roboto inline-block md:text-base"
-							>
-								{category?.name}
-							</Link>
-						))}
-					</div>
-					<h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
-						{data?.title}
-					</h1>
-					<div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
-					<CommentsContainer className="mt-10" loggedInUserId="a" />
-				</article>
-				<div>
-					<SuggestedPost
-						header="Latest articles"
-						posts={postsdata}
-						tags={tagsData}
-						className="mt-8 lg:mt-0 lg:max-w-xs"
-					/>
-					<div className="mt-7">
-						<h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
-							Share on:
-						</h2>
-						<SocialShareButtons
-							url={encodeURI(
-								'https://moonfo.com/post/client-side-and-server-side-explanation'
-							)}
-							title={encodeURIComponent(
-								'Client-side and Server-side explanation'
-							)}
+			{isLoading ? (
+				<ArticleDetailSkeleton />
+			) : isError ? (
+				<ErrorMessage message="Something went wrong, please try again later." />
+			) : (
+				<section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+					<article className="flex-1">
+						<BreadCrumbs data={breadCrumbsData} />
+						<img
+							className="rounded-xl w-full"
+							src={
+								data?.photo
+									? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
+									: images.SamplePostImage
+							}
+							alt={data?.title}
 						/>
+						<div className="mt-4 flex gap-3">
+							{data?.categories?.map(category => (
+								<Link
+									key={category?.name}
+									to={`/blog?category=${category?.name}`}
+									className="text-primary text-sm font-roboto inline-block md:text-base"
+								>
+									{category?.name}
+								</Link>
+							))}
+						</div>
+						<h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
+							{data?.title}
+						</h1>
+						<div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
+						<CommentsContainer className="mt-10" loggedInUserId="a" />
+					</article>
+					<div>
+						<SuggestedPost
+							header="Latest articles"
+							posts={postsdata}
+							tags={tagsData}
+							className="mt-8 lg:mt-0 lg:max-w-xs"
+						/>
+						<div className="mt-7">
+							<h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
+								Share on:
+							</h2>
+							<SocialShareButtons
+								url={encodeURI(
+									'https://moonfo.com/post/client-side-and-server-side-explanation'
+								)}
+								title={encodeURIComponent(
+									'Client-side and Server-side explanation'
+								)}
+							/>
+						</div>
 					</div>
-				</div>
-			</section>
+				</section>
+			)}
 		</MainLayout>
 	);
 };
