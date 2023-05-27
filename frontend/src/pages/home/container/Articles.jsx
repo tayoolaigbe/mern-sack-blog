953f80c4cd9a50d2201d6acf3e5f3630d3e23f47
tayoolaigbe <tayoolaigbe@outlook.com>
2023-05-27 22:58:45 +0100

    Implemented the loading state functionality

--- frontend/src/pages/home/container/Articles.jsx
+++ frontend/src/pages/home/container/Articles.jsx
@@ -4,6 +4,7 @@ import ArticleCard from '../../../components/ArticleCard';
 import { useQuery } from '@tanstack/react-query';
 import { getAllPosts } from '../../../services/index/post';
 import { toast } from 'react-hot-toast';
+import ArticleCardSkeleton from '../../../components/ArticleCardSkeleton';
 
 const Articles = () => {
 	const { data, isLoading, isError } = useQuery({
@@ -17,15 +18,20 @@ const Articles = () => {
 	return (
 		<section className="flex flex-col container mx-auto px-5 py-10">
 			<div className=" flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
-				{!isLoading &&
-					!isError &&
-					data?.map(post => (
-						<ArticleCard
-							key={post._id}
-							post={post}
-							className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
-						/>
-					))}
+				{isLoading
+					? [...Array(3)].map((item, index) => (
+							<ArticleCardSkeleton
+								key={index}
+								className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
+							/>
+					  ))
+					: data?.map(post => (
+							<ArticleCard
+								key={post._id}
+								post={post}
+								className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
+							/>
+					  ))}
 			</div>
 			<button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
 				<span>More articles</span>
